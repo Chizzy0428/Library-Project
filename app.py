4eb70043f53c0f4895bb0f06ad25138b6fa21cd3
Nwabuisi Chizaram F <137488570+Chizzy0428@users.noreply.github.com>
2025-03-03 15:56:01 +0000

    update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 import streamlit as st
 import random
 
